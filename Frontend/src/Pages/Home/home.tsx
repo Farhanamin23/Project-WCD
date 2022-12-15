f2383b9cb9d4b3d9d428bfd402ffc3fbd54215b6
hendrosetwn <hendro.setiawan1995@gmail.com>
2022-12-15 22:24:13 +0700

    add navbar dan home section global partner

--- Frontend/src/Pages/Home/home.tsx
+++ Frontend/src/Pages/Home/home.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./home.scss";
+
+const unesco = require("../../images/home/unesco.png");
+const habitat = require("../../images/home/habitat.png");
+const globe = require("../../images/home/globe.png");
 
 const Home: React.FC = () => {
   return (
-    <>
-      <h1>
-        Welcome To Home Page Boss. Not have a account ser?
-        <Link to={"/login"}>
-          {" "}
-          <u>Register</u>
-        </Link>
-      </h1>
-    </>
+    <div className="homepage">
+      <div className="homepage__globalpartner">
+        <h1>Global Partner</h1>
+        <div className="homepage__globalpartner--logo">
+          <img className="unesco" src={unesco} alt="" />
+          <p>
+            #break<span>free</span>fromplastic
+          </p>
+          <img className="habitat" src={habitat} alt="" />
+          <img className="globe" src={globe} alt="" />
+        </div>
+        <a href="/">View all partners</a>
+      </div>
+    </div>
   );
 };
 
--- Frontend/src/component/navbar/navbar.tsx
+++ Frontend/src/component/navbar/navbar.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./navbar.scss";
 
-const Navbar:React.FC = () => {
+const img_logo = require("../../images/navbar/logo.png");
+
+const Navbar: React.FC = () => {
   return (
-    <>
-    <h1>This is Navbar</h1>
-    </>
-  )
-}
+    <div className="navbar">
+      <img className="navbar__img" src={img_logo} alt="" />
+      <nav className="navbar__nav">
+        <a href="/">Home</a>
+        <a href="/">About Us</a>
+        <a href="/">Program</a>
+        <a href="/">Donate</a>
+        <a href="/">Contact Us</a>
+        <a href="/">Join the movement</a>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
